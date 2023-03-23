56e670ee28ca536bafda94a89ba6941fc6943438
Maaike Wolters <maaikeswolters@gmail.com>
2023-03-23 15:12:45 +0100

    Dependencies update

--- composeapp/build.gradle.kts
+++ composeapp/build.gradle.kts
@@ -1,11 +1,13 @@
+@file:Suppress("UnstableApiUsage")
+
 plugins {
     id("com.android.application")
     kotlin("android")
     id("kotlin-parcelize")
 }
 
-val composeVersion = "1.3.1"
-val accompanistVersion  = "0.28.0"
+val composeVersion = "1.4.0"
+val accompanistVersion  = "0.30.0"
 
 android {
     namespace = "com.move4mobile.lichtstad"
@@ -36,7 +38,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.3.2"
+        kotlinCompilerExtensionVersion = "1.4.4"
     }
 
     flavorDimensions += "whitelabel"
@@ -65,11 +67,11 @@ dependencies {
     implementation("com.google.accompanist:accompanist-systemuicontroller:$accompanistVersion")
     implementation("com.google.accompanist:accompanist-pager:$accompanistVersion")
 
-    implementation("androidx.activity:activity-compose:1.6.1")
+    implementation("androidx.activity:activity-compose:1.7.0")
 
     implementation("androidx.navigation:navigation-compose:2.5.3")
 
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
 
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
 
