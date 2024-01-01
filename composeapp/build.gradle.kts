e0c99ab4850497e3353ceb3ce6979c09b9e96af9
Maaike Wolters <maaikeswolters@gmail.com>
2024-01-01 13:17:49 +0100

    Update navigation-compose
    A strange animation happens when switching from a tab with content to one without. I assume that gets fixed when the other tabs get filled. If not, I'm sorry to later me

--- composeapp/build.gradle.kts
+++ composeapp/build.gradle.kts
@@ -77,7 +77,7 @@ dependencies {
 
     implementation("androidx.activity:activity-compose:1.8.2")
 
-    implementation("androidx.navigation:navigation-compose:2.5.3")
+    implementation("androidx.navigation:navigation-compose:2.7.6")
 
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.1")
--- composeapp/src/main/java/nl/drbreakalot/lichtstad/ui/program/ProgramContent.kt
+++ composeapp/src/main/java/nl/drbreakalot/lichtstad/ui/program/ProgramContent.kt
@@ -1,22 +1,22 @@
 package nl.drbreakalot.lichtstad.ui.program
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.*
-
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Tab
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import kotlinx.coroutines.launch
 import kotlinx.datetime.LocalDate
-import nl.drbreakalot.lichtstad.data.model.DemoProgram
 import nl.drbreakalot.lichtstad.ui.component.CenterableScrollableTabRow
 import nl.drbreakalot.lichtstad.ui.formatTabDate
 import nl.drbreakalot.lichtstad.ui.theme.LichtstadTheme
@@ -49,7 +49,6 @@ fun ProgramContent(viewModel: ProgramViewModel = koinNavViewModel()) {
                 state = pagerState
             ) { page ->
                 ProgramDay(day = days[page])
-
             }
         }
     }
