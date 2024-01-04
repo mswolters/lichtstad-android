7f1910de7c9a03962ac5c066c0c6cb965b8398e8
Maaike Wolters <maaikeswolters@gmail.com>
2024-01-04 14:02:21 +0100

    Add loading indicator to video card

--- composeapp/src/main/java/nl/drbreakalot/lichtstad/ui/video/VideoItem.kt
+++ composeapp/src/main/java/nl/drbreakalot/lichtstad/ui/video/VideoItem.kt
@@ -1,20 +1,28 @@
 package nl.drbreakalot.lichtstad.ui.video
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.SubcomposeAsyncImage
 import coil.compose.rememberAsyncImagePainter
 import nl.drbreakalot.lichtstad.R
 import nl.drbreakalot.lichtstad.data.model.DemoVideo
@@ -35,14 +43,20 @@ fun VideoItem(
             .then(modifier)
     ) {
         Column(modifier = Modifier.clickable { onClick() }) {
-            Image(
+            SubcomposeAsyncImage(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .aspectRatio(16f / 9f),
-                painter = rememberAsyncImagePainter(
-                    video.thumbnails["high"],
-                    contentScale = ContentScale.Crop
-                ),
+                    .aspectRatio(16f / 8.5f),
+                model = video.thumbnails["high"],
+                loading = {
+                    Box(modifier = Modifier) {
+                        CircularProgressIndicator(
+                            modifier = Modifier
+                                .size(40.dp)
+                                .align(Alignment.Center)
+                        )
+                    }
+                },
                 contentDescription = stringResource(
                     R.string.video_item_image_description_pattern,
                     video.title
