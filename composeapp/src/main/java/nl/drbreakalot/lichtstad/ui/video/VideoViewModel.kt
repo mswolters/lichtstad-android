b8ac29cabdb500a8fc2f2de55accc9530fb71912
Maaike Wolters <maaikeswolters@gmail.com>
2024-01-04 14:22:46 +0100

    Make video click open video

--- composeapp/src/main/java/nl/drbreakalot/lichtstad/ui/video/VideoViewModel.kt
+++ composeapp/src/main/java/nl/drbreakalot/lichtstad/ui/video/VideoViewModel.kt
@@ -1,12 +1,16 @@
 package nl.drbreakalot.lichtstad.ui.video
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import android.content.Intent
+import android.net.Uri
+import androidx.lifecycle.AndroidViewModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import nl.drbreakalot.lichtstad.data.model.Video
 import nl.drbreakalot.lichtstad.data.service.VideoService
 
-class VideoViewModel(private val service: VideoService) : ViewModel() {
+class VideoViewModel(application: Application, private val service: VideoService) :
+    AndroidViewModel(application) {
 
     val years = service.years
     fun videos(year: Int): Flow<List<Video>> {
@@ -14,7 +18,9 @@ class VideoViewModel(private val service: VideoService) : ViewModel() {
     }
 
     fun onClicked(video: Video) {
-        //TODO
+        val uri = Uri.parse("http://www.youtube.com/watch?v=${video.id}")
+        val intent = Intent(Intent.ACTION_VIEW, uri).apply { flags = Intent.FLAG_ACTIVITY_NEW_TASK }
+        getApplication<Application>().startActivity(intent)
     }
 
 }
