1273e7ae015e97817ea83ea89160801b8516d648
Maaike Wolters <maaikeswolters@gmail.com>
2024-02-13 13:50:41 +0100

    Fix animation when opening photo album

--- composeapp/src/main/java/nl/drbreakalot/lichtstad/ui/photo/PhotoList.kt
+++ composeapp/src/main/java/nl/drbreakalot/lichtstad/ui/photo/PhotoList.kt
@@ -3,9 +3,11 @@ package nl.drbreakalot.lichtstad.ui.photo
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
@@ -19,11 +21,13 @@ import org.koin.androidx.compose.navigation.koinNavViewModel
 @Composable
 fun Album(year: String, key: String, viewModel: PhotoViewModel = koinNavViewModel()) {
     val photos by viewModel.photos(year, key).collectAsStateWithLifecycle(emptyList())
-    PhotoList(photos = photos, onPhotoClick = { })
+    Scaffold { paddingValues ->
+        PhotoList(modifier = Modifier.padding(paddingValues), photos = photos, onPhotoClick = { })
+    }
 }
 
 @Composable
-fun PhotoList(photos: List<Photo>, onPhotoClick: (Photo) -> Unit) {
+fun PhotoList(modifier: Modifier, photos: List<Photo>, onPhotoClick: (Photo) -> Unit) {
     LazyVerticalGrid(
         columns = GridCells.Adaptive(80.dp),
         verticalArrangement = Arrangement.spacedBy(4.dp),
